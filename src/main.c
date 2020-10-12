4d515fe71279c1e071b823d070f40ac263addb17
Ankur Seth <iankurseth@gmail.com>
2020-10-12 18:22:07 +0530

    Now listing entries

--- src/main.c
+++ src/main.c
@@ -6,13 +6,34 @@
 #define CREATE_SINK(x) struct Sink *x; x = getNewSink();
 #define DELETE_SINK(x) deleteSink(x);x=NULL;
 
-int debug = 1;
+#define MAX_CHAR 500
+
+#define true 1
+#define false 0
+#define bool short
+
+int debug = 0;
 
 char sinkfile_names[3][10] = {
 	"sinkfile",
 	"sink.sink",
 	"sink"
 };
+
+int trim_right (char *str) {
+	int n = 0;
+	int last_nonspace_index = 0;
+	char *ch = str;
+	while (*(++ch) != '\0') {
+		n++;
+		if (*ch != ' ') {
+			last_nonspace_index = n;
+		}
+	}
+
+	str[last_nonspace_index+1] = '\0';
+	return last_nonspace_index;
+}
 
 struct SinkableObject {
 	char *local_path;
@@ -50,6 +71,63 @@ struct Sink *getNewSink () {
 	}
 
 	struct Sink *sink = malloc(sizeof (struct Sink));
+
+	char s1[MAX_CHAR] = "";
+	char s2[MAX_CHAR] = "";
+
+	int n1 = 0;
+	int n2 = 0;
+
+	int line_number = 1;
+	bool equals_found = false;
+
+	char ch;
+	while (true) {
+		ch = fgetc(fp);
+
+		if (ch == '\n' || ch == EOF) {
+			s1[n1] = '\0';
+			s2[n2] = '\0';
+
+			trim_right(s1);
+			trim_right(s2);
+
+			if (equals_found) {
+				printf("[L%d] '%s' =[%d]= '%s'\n", line_number, s1, equals_found, s2);
+			}
+
+			s1[0] = '\0';
+			s2[0] = '\0';
+			n1 = 0;
+			n2 = 0;
+
+			if (ch == EOF) {
+				break;
+			} else {
+				line_number++;
+				equals_found = false;
+			}
+		} else {
+			if (!equals_found && ch == '=') {
+				equals_found = true;
+			} else {
+				if (equals_found){
+					if (ch == ' ' && n2 == 0) {
+						//
+					} else {
+						s2[n2++] = ch;
+					}
+				} else {
+					if (ch == ' ' && n1 == 0) {
+						//
+					} else {
+						s1[n1++] = ch;
+					}
+				}
+			}
+		}
+	}
+
 	fclose(fp);
 	return sink;
 }
