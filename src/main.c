4027719f75b0ea7ac5a042fd12b027e77165dcf7
Ankur Seth <iankurseth@gmail.com>
2020-10-12 15:37:55 +0530

    Now selecting proper sinkfile

--- src/main.c
+++ src/main.c
@@ -1,7 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define DEBUG_MESSAGE(...) if (debug == 1) {printf(__VA_ARGS__);}
+
+int debug = 1;
+
+char sinkfile_names[3][10] = {
+	"sinkfile",
+	"sink.sink",
+	"sink"
+};
+
+struct SinkableObject {
+	char *local_path;
+	char *remote_path;
+	char *remote_content;
+
+	struct SinkableObject *next;
+	struct SinkableObject *prev;
+};
+
+struct Sink {
+	struct SinkableObject *first_object;
+};
+
+struct Sink *getNewSink () {
+	FILE *fp = fopen(sinkfile_names[0], "r");
+
+	if (fp == NULL) {
+		DEBUG_MESSAGE("Not found: %s\n", sinkfile_names[0]);
+		fp = fopen(sinkfile_names[1], "r");
+		if (fp == NULL) {
+			DEBUG_MESSAGE("Not found: %s\n", sinkfile_names[1]);
+			fp = fopen(sinkfile_names[2], "r");
+			if (fp == NULL) {
+				DEBUG_MESSAGE("Not found: %s\n", sinkfile_names[2]);
+				return NULL;
+			} else {
+				DEBUG_MESSAGE("Found: %s\n", sinkfile_names[2]);
+			}
+		} else {
+			DEBUG_MESSAGE("Found: %s\n", sinkfile_names[1]);
+		}
+	} else {
+		DEBUG_MESSAGE("Found: %s\n", sinkfile_names[0]);
+	}
+	return NULL;
+}
+
+void printSink (struct Sink *sink) {
+	//
+}
 
 int main(int argc, char const *argv[])
 {
-	printf("Hello, World!\n");
+	struct Sink *sink;
+	sink = getNewSink();
 	return 0;
 }
